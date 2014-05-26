2d984712f1945ee68163cdfd1d8def87516b7583
TT <edy555@gmail.com>
2014-05-27 08:35:01 +0900

    fix knob position, add comment to connect ui and controllers

--- extension/interface-jqskin.js
+++ extension/interface-jqskin.js
@@ -1,3 +1,4 @@
+//function constructSkin(interface) {
 $(function() {
 	$('<img />').panel({
 		id: 'panel',
@@ -10,8 +11,8 @@ $(function() {
 	$('<img />').switch({
 		id: 'led',
 		image: 'images/led_big_white.png',
-		left: 352,
-		top: 75,
+		left: 33,
+		top: 416,
 		width: 32,
 		height: 32,
 		value: 0,
@@ -22,8 +23,8 @@ $(function() {
 	$('<img />').switch({
 		id: 'sw',
 		image: 'images/toggle_sw.png',
-		left: 190,
-		top: 370,
+		left: 170,
+		top: 350,
 		width: 32,
 		height: 32,
 		value: 0,
@@ -36,14 +37,14 @@ $(function() {
 	// Volume
 	$('<img />').knob({
 		id: 'knob37', image: 'images/knob_white_big_mid.png',
-		left: 80, top: 320, width: 60, height: 60, value: 40,
+		left: 60, top: 300, width: 60, height: 60, value: 40,
 		change: (function() {var v = Math.floor($(this).knob("value") / 51); $('#led04').switch("value", v); })
 	}).appendTo('#draw');
 
 	// Band Switch
 	$('<img />').knob({
 		id: 'knob25', image: 'images/knob_white_big_mid.png',
-		left: 550, top: 310, width: 60, height: 60, value: 50
+		left: 530, top: 290, width: 60, height: 60, value: 50
 	}).appendTo('#draw');
 
 });
